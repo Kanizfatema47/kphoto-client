ed7b9fb1fc0e4795887db99082f9db7a8b459c35
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-04-18 19:27:43 +0600

    toast after clicking reset password

--- src/SignUp/Login/Login.js
+++ src/SignUp/Login/Login.js
@@ -1,9 +1,11 @@
 import React, { useRef } from 'react';
-import { Button, Form} from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword, useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const [
@@ -11,31 +13,43 @@ const Login = () => {
         user,
         loading,
         error,
-      ] = useSignInWithEmailAndPassword(auth);
-      let errorElement;
-      const emailRef = useRef();
-      const passwordRef = useRef();
+    ] = useSignInWithEmailAndPassword(auth);
+    let errorElement;
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+    const navigate = useNavigate();
 
 
-      const navigate = useNavigate();
 
-  
+    if (user) {
+        navigate('/checkout');
+    }
 
-      if(user){
-        navigate ('/checkout');
-      }
-
-      if (error) {
+    if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
 
 
-      const handleSubmit =(event)=>{
-          event.preventDefault();
-          const email = emailRef.current.value;
-          const password = passwordRef.current.value;
-          signInWithEmailAndPassword(email, password)
-      }
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Sent email');
+        }
+        else{
+            toast('please enter your email address');
+        }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password)
+    }
 
     return (
         <div>
@@ -52,12 +66,13 @@ const Login = () => {
                     <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
                         Login
                     </Button>
-                    
+
                 </Form>
-                    {errorElement}
+                {errorElement}
                 <p>New? <Link to="/register" className='text-primary pe-auto text-decoration-none' >Please Register</Link> </p>
-                <p>Forget Password? <button  className='btn btn-link text-primary pe-auto text-decoration-none' >Reset Password</button> </p>
+                <p>Forget Password? <button onClick={resetPassword} className='btn btn-link text-primary pe-auto text-decoration-none' >Reset Password</button> </p>
                 <SocialLogin></SocialLogin>
+                <ToastContainer></ToastContainer>
 
             </div>
         </div>
